7b5dff73080dfe3d0beb0f99c09457708b4ee501
jakobie <jacobwturnbul@gmail.com>
2017-11-06 14:41:56 -0700

    changes to participant and datePref

--- src/structures/Participant.java
+++ src/structures/Participant.java
@@ -13,19 +13,31 @@ public class Participant extends user {
     }
 
     /**
-     * 
+     * Name of Participant
      */
     private String Name;
 
     /**
-     * 
+     * Is the Participant important for this meeting
      */
     private Boolean Important;
 
     /**
-     * 
+     * The Username of the Participant linking them to the user
      */
     private String UserName;
-
-
+    
+    /**
+     * 
+     */
+    public void setName(String n){
+    	Name = n;
+    }
+    /**
+     * 
+     */
+    public String getName(){
+    	return Name;
+    }
+    public void setImportant(Boolean)
 }
--- src/structures/datePref.java
+++ src/structures/datePref.java
@@ -24,4 +24,6 @@ public class DatePref {
     private ArrayList<Date> prefDates;
 
     
+    
+    
 }
