282ec71167138d3b8fd2cf006db20ceeb085065c
Jeremy Phillips <jeremy.phillips@ucalgary.ca>
2017-11-17 10:43:04 -0700

    ReCommit

--- src/server/MeetingList.java
+++ src/server/MeetingList.java
@@ -37,15 +37,27 @@ public class MeetingList {
 		schedulerGo = true;
 	}
 	
-	/*
+	
 	// For testing purposes only
 	public static void main(String [] args){
-		MeetingList u = MeetingList.getMeetingList();
+		MeetingList ml = MeetingList.getMeetingList();
 		Meeting m = new Meeting();
-		m.setID(1234);
-		u.addMeeting(m);
+		
+		UserList ul = UserList.getUserList();
+		ArrayList<User> u = ul.getUsers();
+		
+		m.setMeetingInitiator(u.get(u.size() - 1));
+		
+		for (int i = 0; i < u.size(); i++){
+			m.addParticipant(u.get(i).getUserName(), u.get(i).getName(), true);
+		}
+		
+		m.setmeetingState(Meeting.Finalized);
+		m.setID(9);
+		
+		ml.addMeeting(m);
 	}
-	*/
+	
 	
 	
 	/** get the only instance
